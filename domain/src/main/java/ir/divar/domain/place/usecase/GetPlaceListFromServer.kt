d9ebd9e6ffa9970f657fea7e3959e40586ee2415
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-06 21:33:47 +0430

    add GetPlaceListFromServer usecase.

---
+++ domain/src/main/java/ir/divar/domain/place/usecase/GetPlaceListFromServer.kt
@@ -0,0 +1,18 @@
+package ir.divar.domain.place.usecase
+
+import ir.divar.domain.common.GeneralUsecase
+import ir.divar.domain.place.IPlacesRepository
+import ir.divar.domain.place.model.LatLng
+import ir.divar.domain.place.model.Place
+import ir.divar.domain.remote.BaseResult
+import javax.inject.Inject
+
+class GetPlaceListFromServer @Inject constructor(
+    private val iPlacesRepository: IPlacesRepository
+) : GeneralUsecase<GetPlaceListFromServer.RequestValues, BaseResult<List<Place>, String>>() {
+
+    data class RequestValues(val latLng: LatLng) : RequestValue
+
+    override suspend fun executeUseCase(requestValues: RequestValues) =
+        iPlacesRepository.getPlaces(requestValues.latLng)
+}
