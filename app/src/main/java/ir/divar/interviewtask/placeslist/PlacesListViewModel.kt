e07aa0d2b2f72a71c28e99fdfa60f9aee7dc3641
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-07 01:59:29 +0430

    add related usecases to PlacesListViewModel.

--- app/src/main/java/ir/divar/interviewtask/placeslist/PlacesListViewModel.kt
+++ app/src/main/java/ir/divar/interviewtask/placeslist/PlacesListViewModel.kt
@@ -1,10 +1,74 @@
 package ir.divar.interviewtask.placeslist
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ir.divar.domain.place.model.Place
+import ir.divar.domain.place.usecase.GetPlaceListFromLocal
+import ir.divar.domain.place.usecase.GetPlaceListFromServer
+import ir.divar.domain.place.usecase.InsertPlaceList
+import ir.divar.domain.remote.BaseResult
+import ir.divar.interviewtask.util.Constants.PROBLEM_OCCURRED_ERROR_MESSAGE
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import ir.divar.domain.place.model.LatLng as PlaceModelLatLng
 
 @HiltViewModel
-class PlacesListViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class PlacesListViewModel @Inject constructor(
+    private val getPlaceListFromLocal: GetPlaceListFromLocal,
+    private val getPlaceListFromServer: GetPlaceListFromServer,
+    private val insertPlaceList: InsertPlaceList
+) : ViewModel() {
+
+    val getLocalPlaceListResponse = MutableLiveData<List<Place>>()
+    val getLocalPlaceListError = MutableLiveData<Unit>()
+
+    val getServerPlaceListResponse = MutableLiveData<List<Place>>()
+    val getServerPlaceListError = MutableLiveData<String>()
+
+    val insertPlaceListResponse = MutableLiveData<Unit>()
+    val insertPlaceListError = MutableLiveData<Unit>()
+
+    /** handles exceptions may be occurred in coroutine scopes */
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, e ->
+        e.printStackTrace()
+    }
+
+    fun getLocalPlaceList() {
+        viewModelScope.launch(coroutineExceptionHandler) {
+            getPlaceListFromLocal.executeUseCase(GetPlaceListFromLocal.RequestValues())?.also {
+                getLocalPlaceListResponse.value = it
+            } ?: apply { getLocalPlaceListError.value = Unit }
+        }
+    }
+
+    fun getServerPlaceList(latLng: LatLng) {
+        viewModelScope.launch(coroutineExceptionHandler) {
+            getPlaceListFromServer.executeUseCase(
+                GetPlaceListFromServer.RequestValues(
+                    PlaceModelLatLng(latLng.latitude, latLng.longitude)
+                )
+            ).also {
+                when (it) {
+                    is BaseResult.Success -> it.data?.also { placeList ->
+                        getServerPlaceListResponse.value = placeList
+                    } ?: apply { getServerPlaceListError.value = PROBLEM_OCCURRED_ERROR_MESSAGE }
+
+                    is BaseResult.Error -> getServerPlaceListError.value =
+                        it.message ?: PROBLEM_OCCURRED_ERROR_MESSAGE
+                }
+            }
+        }
+    }
+
+    fun insertPlaceList(placeList: List<Place>) {
+        viewModelScope.launch(coroutineExceptionHandler) {
+            insertPlaceList.executeUseCase(InsertPlaceList.RequestValues(placeList))?.also {
+                insertPlaceListResponse.value = it
+            } ?: apply { insertPlaceListError.value = Unit }
+        }
+    }
 }
--- app/src/main/java/ir/divar/interviewtask/util/Constants.kt
+++ app/src/main/java/ir/divar/interviewtask/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val DATABASE_NAME = "divar_interview_task_database"
     const val BASE_URL = "https://api.foursquare.com/v3/"
+
+    const val PROBLEM_OCCURRED_ERROR_MESSAGE = "A Problem Occurred in Server Response"
 }
