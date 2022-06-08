b7345b462377144e5322bb6f8e3548a07c167841
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-08 18:34:53 +0430

    add map to PlaceDetailsFragment.

--- app/src/main/java/ir/divar/interviewtask/placedetails/PlaceDetailsFragment.kt
+++ app/src/main/java/ir/divar/interviewtask/placedetails/PlaceDetailsFragment.kt
@@ -1,13 +1,24 @@
 package ir.divar.interviewtask.placedetails
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 import ir.divar.interviewtask.R
 import ir.divar.interviewtask.databinding.FragmentPlaceDetailsBinding
@@ -19,13 +30,15 @@ import ir.divar.interviewtask.databinding.FragmentPlaceDetailsBinding
  * @author Sepi 6/4/22
  */
 @AndroidEntryPoint
-class PlaceDetailsFragment : Fragment(), View.OnClickListener {
+class PlaceDetailsFragment : Fragment(), View.OnClickListener, OnMapReadyCallback {
 
     // region of properties
     private val placeDetailsViewModel by viewModels<PlaceDetailsViewModel>()
     private val args by navArgs<PlaceDetailsFragmentArgs>()
     private var _binding: FragmentPlaceDetailsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var map: GoogleMap
 
     // END of region of properties
 
@@ -35,6 +48,10 @@ class PlaceDetailsFragment : Fragment(), View.OnClickListener {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPlaceDetailsBinding.inflate(inflater, container, false)
+
+        (childFragmentManager.findFragmentById(R.id.fcvPlaceMap) as SupportMapFragment)
+            .getMapAsync(this)
+
         return binding.root
     }
 
@@ -44,7 +61,7 @@ class PlaceDetailsFragment : Fragment(), View.OnClickListener {
         initialView()
     }
 
-    fun initialView() {
+    private fun initialView() {
         with(binding) {
             with(args.place) {
                 tvPlaceDistance.text = distance.toString()
@@ -57,7 +74,55 @@ class PlaceDetailsFragment : Fragment(), View.OnClickListener {
                 )
                 tvPlaceAddress.text = location.formattedAddress
 
+                addPlaceMarker(geocodes.main.let { LatLng(it.latitude, it.longitude) })
+
                 imgPlaceBack.setOnClickListener(this@PlaceDetailsFragment)
+            }
+        }
+    }
+
+    private fun addPlaceMarker(latLng: LatLng) {
+        map.addMarker(
+            MarkerOptions().apply {
+                position(latLng)
+                icon(
+                    BitmapDescriptorFactory.fromBitmap(
+                        ResourcesCompat.getDrawable(
+                            resources,
+                            R.drawable.ic_pin,
+                            requireContext().theme
+                        )!!.toBitmap()
+                    )
+                )
+            })
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.apply {
+            mapType = GoogleMap.MAP_TYPE_NORMAL
+            isMyLocationEnabled = true
+
+            // move camera to current location bindings to display user and place location in a frame
+            moveCamera(
+                CameraUpdateFactory.newLatLngBounds(
+                    LatLngBounds.Builder().apply {
+                        include(args.place.geocodes.main.let { LatLng(it.latitude, it.longitude) })
+                        include(args.currentLocation)
+                    }.build(),
+                    12
+                )
+            )
+
+            uiSettings.apply {
+                isMyLocationButtonEnabled = true
+                isRotateGesturesEnabled = true
+                isScrollGesturesEnabled = true
+                isMapToolbarEnabled = true
+                isZoomControlsEnabled = true
+                isZoomGesturesEnabled = true
+                isCompassEnabled = true
             }
         }
     }
