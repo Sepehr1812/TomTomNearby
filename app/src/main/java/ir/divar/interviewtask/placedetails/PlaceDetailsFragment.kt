ca8fefe791c77756dd6c8de67b432e4efcaa0e94
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-08 18:18:21 +0430

    initialize view in PlaceDetailsFragment.

--- app/src/main/java/ir/divar/interviewtask/placedetails/PlaceDetailsFragment.kt
+++ app/src/main/java/ir/divar/interviewtask/placedetails/PlaceDetailsFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
+import ir.divar.interviewtask.R
 import ir.divar.interviewtask.databinding.FragmentPlaceDetailsBinding
 
 
@@ -16,10 +19,11 @@ import ir.divar.interviewtask.databinding.FragmentPlaceDetailsBinding
  * @author Sepi 6/4/22
  */
 @AndroidEntryPoint
-class PlaceDetailsFragment : Fragment() {
+class PlaceDetailsFragment : Fragment(), View.OnClickListener {
 
     // region of properties
     private val placeDetailsViewModel by viewModels<PlaceDetailsViewModel>()
+    private val args by navArgs<PlaceDetailsFragmentArgs>()
     private var _binding: FragmentPlaceDetailsBinding? = null
     private val binding get() = _binding!!
 
@@ -34,8 +38,38 @@ class PlaceDetailsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initialView()
+    }
+
+    fun initialView() {
+        with(binding) {
+            with(args.place) {
+                tvPlaceDistance.text = distance.toString()
+                tvPlaceCategory.text = categories
+                tvPlaceTitle.text = name
+                tvPlaceRegion.text = getString(
+                    R.string.region_country_placeholder,
+                    location.region,
+                    location.country
+                )
+                tvPlaceAddress.text = location.formattedAddress
+
+                imgPlaceBack.setOnClickListener(this@PlaceDetailsFragment)
+            }
+        }
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            binding.imgPlaceBack.id -> findNavController().navigateUp()
+        }
     }
 }
