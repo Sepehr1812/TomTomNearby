15692cd5ac745ab8c403c42df2ca7bea11d8f563
Sepehr Akhoundi <sepehrakhoundi1812@gmail.com>
2022-06-08 15:50:28 +0430

    initial fragment_place_details.

--- app/src/main/java/ir/divar/interviewtask/activity/MainActivity.kt
+++ app/src/main/java/ir/divar/interviewtask/activity/MainActivity.kt
@@ -3,13 +3,22 @@ package ir.divar.interviewtask.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import ir.divar.interviewtask.R
+import ir.divar.interviewtask.databinding.ActivityMainBinding
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // region of properties
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    // END of region of properties
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
